c48f984a1ad0dbb5efee0fead1a5737bd2ce81c7
Siim Haugas <siim@haugas.com>
2022-05-21 11:07:03 +0300

    Add traits mock data

---
+++ src/mockupData/traits.ts
@@ -0,0 +1,103 @@
+import { TraitWithUniqueScore } from "../types"
+
+const traits: TraitWithUniqueScore[] = [
+  {
+    id: 1,
+    property: 'background',
+    value: 'mint',
+    uniqueScore: 0.005,
+  },
+  {
+    id: 2,
+    property: 'body',
+    value: 'tribal necklace',
+    uniqueScore: 0.04,
+  },
+  {
+    id: 3,
+    property: 'face',
+    value: 'clout goggles',
+    uniqueScore: 0.5,
+  },
+  {
+    id: 4,
+    property: 'ear',
+    value: 'big',
+    uniqueScore: 0.02,
+  },
+  {
+    id: 5,
+    property: 'background',
+    value: 'black',
+    uniqueScore: 0.0051,
+  },
+  {
+    id: 6,
+    property: 'body',
+    value: 'tribal shirt',
+    uniqueScore: 0.042,
+  },
+  {
+    id: 7,
+    property: 'face',
+    value: 'nice goggles',
+    uniqueScore: 0.54,
+  },
+  {
+    id: 8,
+    property: 'ear',
+    value: 'small',
+    uniqueScore: 0.52,
+  },
+
+  {
+    id: 9,
+    property: 'face',
+    value: 'glasses',
+    uniqueScore: 0.19,
+  },
+  {
+    id: 10,
+    property: 'face',
+    value: 'large nose',
+    uniqueScore: 12,
+  },
+  {
+    id: 11,
+    property: 'body',
+    value: 'light armor',
+    uniqueScore: 50,
+  },
+
+  {
+    id: 12,
+    property: 'body',
+    value: 'rain jacket',
+    uniqueScore: 7,
+  },
+
+  {
+    id: 13,
+    property: 'background',
+    value: 'skyblue',
+    uniqueScore: 140,
+  },
+
+  {
+    id: 14,
+    property: 'ear',
+    value: 'pearl',
+    uniqueScore: 0.019,
+  },
+
+  {
+    id: 15,
+    property: 'ear',
+    value: 'cat',
+    uniqueScore: 0.011,
+  }
+
+
+]
+
+export default traits
