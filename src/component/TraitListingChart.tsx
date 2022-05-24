e3e67fd7b8602e03d9f673b66db7d8c90bd8005b
Tayo Akosile <oluwatayoakosile@gmail.com>
2022-05-24 11:37:24 +0100

    Update:Creating Graph

--- src/component/TraitListingChart.tsx
+++ src/component/TraitListingChart.tsx
@@ -3,103 +3,89 @@ import Chart from "react-apexcharts";
 import { Box } from '@chakra-ui/react';
 
 const TraitListingChart = () => {
+    const series = [{
+        name: 'Inflation',
+        data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2, 0.2]
+    }]
 
-const series = [{
-    name: 'Inflation',
-    data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2, 0.2]
-}]
-
-const options = {
-    chart: {
-        height: 350,
-        type: 'bar',
-    },
-    plotOptions: {
-        bar: {
-            borderRadius: 0,
-            dataLabels: {
-                position: 'top', // top, center, bottom
-            },
-        }
-    },
-    dataLabels: {
-        enabled: true,
-        formatter: function (val: any) {
-            return val + "%";
+    const options = {
+        chart: {
+            height: 350,
+            type: 'bar',
         },
-        offsetY: -20,
-        style: {
-            fontSize: '12px',
-            colors: ["#304758"]
-        }
-    },
-    crosshairs: {
-        fill: {
-            type: 'gradient',
-            gradient: {
-                colorFrom: '#D8E3F0',
-                colorTo: '#000',
-                stops: [0, 100],
-                opacityFrom: 0.4,
-                opacityTo: 0.5,
+        plotOptions: {
+            bar: {
+                borderRadius: 0,
+                dataLabels: {
+                    position: 'top', // top, center, bottom
+                },
             }
-        }
-    },
-
-    xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        position: 'top',
-        axisBorder: {
-            show: false
         },
-        axisTicks: {
-            show: false
-        },
-      
-        tooltip: {
+        dataLabels: {
             enabled: true,
-        }
-    },
-    yaxis: {
-        axisBorder: {
-            show: false
-        },
-        axisTicks: {
-            show: false,
-        },
-        labels: {
-            show: false,
             formatter: function (val: any) {
                 return val + "%";
+            },
+            offsetY: -20,
+            style: {
+                fontSize: '12px',
+                colors: ["#304758"]
             }
-        }
+        },
+        crosshairs: {
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    colorFrom: '#D8E3F0',
+                    colorTo: '#000',
+                    stops: [0, 10],
+                    opacityFrom: 0.4,
+                    opacityTo: 0.5,
+                }
+            }
+        },
 
-    },
-    title: {
-        text: 'Monthly Inflation in Argentina, 2002',
-        floating: true,
-        offsetY: 330,
-        align: 'center',
-        style: {
-            color: '#444'
-        }
-    }
+        xaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            position: 'top',
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            },
 
-
-
-};
+            tooltip: {
+                enabled: true,
+            }
+        },
+        yaxis: {
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false,
+            },
+            labels: {
+                show: false,
+                formatter: function (val: any) {
+                    return val + "%";
+                }
+            }
+        },
+    };
 
 
 
 
     return (
-        <Box w='100%' mx='auto'>
+        <Box w='70%' mx='auto'>
             <Box
                 as={Chart}
                 options={options}
                 series={series}
                 type="bar"
-                width="450"
+                width="100"
             />
         </Box>
     )
