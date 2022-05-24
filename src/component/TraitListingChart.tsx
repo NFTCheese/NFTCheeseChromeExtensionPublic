fb085aacbf7d43b93a9958a357b651123fbabd3b
Tayo Akosile <oluwatayoakosile@gmail.com>
2022-05-24 13:30:03 +0100

    Cant seem to get around react aoex chart, moving to number 3

--- src/component/TraitListingChart.tsx
+++ src/component/TraitListingChart.tsx
@@ -3,78 +3,93 @@ import Chart from "react-apexcharts";
 import { Box } from '@chakra-ui/react';
 
 const TraitListingChart = () => {
-    const series = [{
-        name: 'Inflation',
-        data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2, 0.2]
-    }]
+    const state = {
+        series: [
+            {
+                name: '',
+                data: [44, 55, 41,]
+            },
+            {
+                name: '',
+                data: [44, 55, 41,]
+            },
+            {
+                name: '',
+                data: [44, 55, 41,]
+            },
+            {
+                name: '',
+                data: [44, 55, 41,]
+            },
+            {
+                name: '',
+                data: [44, 55, 41,]
+            },
+            {
 
-    const options = {
-        chart: {
-            height: 350,
-            type: 'bar',
-        },
-        plotOptions: {
-            bar: {
-                borderRadius: 0,
-                dataLabels: {
-                    position: 'top', // top, center, bottom
+                name: 'two',
+                data: [44, 55, 41,]
+            }
+            ,
+            {
+                data: [53, 32, 33,]
+
+            }]
+        ,
+        options: {
+
+            chart: {
+                height: 350,
+                type: 'bar',
+                bar: {
+                    borderRadius: 0,
+                }
+            },
+            plotOptions: {
+                bar: {
+                    barHeight: '50%'
                 },
-            }
-        },
-        dataLabels: {
-            enabled: true,
-            formatter: function (val: any) {
-                return val + "%";
             },
-            offsetY: -20,
-            style: {
-                fontSize: '12px',
-                colors: ["#304758"]
-            }
-        },
-        crosshairs: {
-            fill: {
-                type: 'gradient',
-                gradient: {
-                    colorFrom: '#D8E3F0',
-                    colorTo: '#000',
-                    stops: [0, 10],
-                    opacityFrom: 0.4,
-                    opacityTo: 0.5,
+            colors: ['#E07D00'],
+            dataLabels: { enabled: false },
+            xaxis: {
+                categories: ["Jan", "Feb", "Red"],
+                position: 'bottom',
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false
+                },
+                tooltip: {
+                    enabled: true,
                 }
-            }
-        },
-
-        xaxis: {
-            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            position: 'top',
-            axisBorder: {
-                show: false
             },
-            axisTicks: {
-                show: false
-            },
-
-            tooltip: {
-                enabled: true,
-            }
-        },
-        yaxis: {
-            axisBorder: {
-                show: false
-            },
-            axisTicks: {
-                show: false,
-            },
-            labels: {
-                show: false,
-                formatter: function (val: any) {
-                    return val + "%";
+            yaxis: {
+                position: 'top',
+                min: 30,
+                max: 50,
+                axisBorder: {
+                    show: true
+                },
+                axisTicks: {
+                    show: false,
+                },
+                labels: {
+                    show: true,
+                    formatter: function (val: any) {
+                        return val + "%";
+                    }
                 }
-            }
-        },
-    };
+            },
+        }
+    }
 
+
+
+
+
+    const { options, series } = state
 
 
 
